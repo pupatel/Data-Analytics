404dd1636a0d00102039002878d4713030469fc9
Parth Patel <pupatel@udel.edu>
2018-11-16 17:39:51 -0500

    Create NLTK-NamedEntityExtraction.py

---
+++ NLTK-NamedEntityExtraction.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+#Created by Parth Patel, DBI @ University of Delaware, Newark, Delaware 19717
+#Date created: 07/08/2018
+
+import nltk
+from nltk.chunk import ne_chunk
+
+##  lemmas and synonyms in WordNet
+
+
+#### GET SYNSETS ######
