d627925217f721ceefdde377bdfa33fd54a7f1c8
Parth Patel <pupatel@udel.edu>
2018-12-08 18:44:40 -0500

    Update Auto-encoder_Image_Reconstruction.py

--- Auto-encoder_Image_Reconstruction.py
+++ Auto-encoder_Image_Reconstruction.py
@@ -11,74 +11,71 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Load images from training set
+#LOAD IMAGES FROM TRAINING SET
 all_images = np.loadtxt('fashion-mnist_train.csv',delimiter=',', skiprows=1)[:,1:]
 #print(all_images.shape)
 
 # printing the array representation of the first image
-print("the array of the first image looks like", all_images[0])
+#print("the array of the first image looks like", all_images[0])
 # printing something that actually looks like an image
-print("and the actual image looks like")
-plt.imshow(all_images[0].reshape(28,28),  cmap='Greys')
-plt.show()
+#print("and the actual image looks like")
+#plt.imshow(all_images[0].reshape(28,28),  cmap='Greys')
+#plt.show()
 
-# Setting up numbers of nuerons/nodes at each in the autoEncoders
+# SETTING UP NUMBER OF NUERONS/NODES AT EACH IN THE AUTOENCODERS
 n_nodes_inputlayer = 784  #encoder
 n_nodes_hiddenlayer1  = 32  #encoder
 n_nodes_hiddenlayer2  = 32  #decoder
 n_nodes_outlayer = 784  #decoder
 
-#ASSIGN WEIGTHS AND BIASES
+#ASSIGN WEIGTHS AND BIASES TO LAYERS
 
-# first hidden layer has 784*32 weights and 32 biases
+# FIRST HIDDEN LAYER HAS 784*32 WEIGHTS AND 32 BIASES
 hidden_1_layer_vals = {'weights':tf.Variable(tf.random_normal([n_nodes_inputlayer,n_nodes_hiddenlayer1])),'biases':tf.Variable(tf.random_normal([n_nodes_hiddenlayer1]))  }
 
-# second hidden layer has 32*32 weights and 32 biases
+# SECOND HIDDEN LAYER HAS 32*32 WEIGHTS AND 32 BIASES
 hidden_2_layer_vals = {'weights':tf.Variable(tf.random_normal([n_nodes_hiddenlayer1, n_nodes_hiddenlayer2])),'biases':tf.Variable(tf.random_normal([n_nodes_hiddenlayer2]))  }
 
-# output hidden layer has 32*784 weights and 784 biases
+# OUTPUT HIDDEN LAYER HAS 32*784 WEIGHTS AND 784 BIASES
 output_layer_vals = {'weights':tf.Variable(tf.random_normal([n_nodes_hiddenlayer2,n_nodes_outlayer])),'biases':tf.Variable(tf.random_normal([n_nodes_outlayer]))}
 
 
-
-# image with shape 784 goes in
+# IMAGE WITH SHAPE 784 GOES IN
 input_layer = tf.placeholder('float', [None, 784])
 
-# multiply output of input_layer wth a weight matrix and add biases
+# MULTIPLY OUTPUT OF INPUT_LAYER WTH A WEIGHT MATRIX AND ADD BIASES
 layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(input_layer,hidden_1_layer_vals['weights']),hidden_1_layer_vals['biases']))
 
-# multiply output of layer_1 wth a weight matrix and add biases
+# MULTIPLY OUTPUT OF LAYER_1 WTH A WEIGHT MATRIX AND ADD BIASES
 layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1,hidden_2_layer_vals['weights']),hidden_2_layer_vals['biases']))
 
-# multiply output of layer_2 wth a weight matrix and add biases
+# MULTIPLY OUTPUT OF LAYER_2 WTH A WEIGHT MATRIX AND ADD BIASES
 output_layer = tf.matmul(layer_2,output_layer_vals['weights']) + output_layer_vals['biases']
 
-# output_true shall have the original image for error calculations
+# OUTPUT_TRUE  HAVE THE ORIGINAL IMAGE FOR ERROR CALCULATIONS
+output_original = tf.placeholder('float', [None, 784])
 
-output_true = tf.placeholder('float', [None, 784])
+# DEFINE COST FUNCTION TO MINIMIZE ERROR
+meansq = tf.reduce_mean(tf.square(output_layer - output_original))
 
-# define our cost function
-meansq =    tf.reduce_mean(tf.square(output_layer - output_true))
-
-# define our optimizer
+# USING ADAM OPTIMIZER
 learn_rate = 0.1   # how fast the model should learn
 optimizer = tf.train.AdagradOptimizer(learn_rate).minimize(meansq)
 
 
-
-# initialising stuff and starting the session
+# INITIATLIZE & START SESSION
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-# defining batch size, number of epochs and learning rate
-batch_size = 100  # how many images to use together for training
-hm_epochs =1000    # how many times to go through the entire dataset
-tot_images = 60000 # total number of images
+# DEFINING BATCH SIZE, NUMBER OF EPOCHS AND LEARNING RATE
+batch_size = 100  # Try 128
+hm_epochs =1000    # This may be too much
+tot_images = 60000 # Total set
 
 
-# running the model for a 1000 epochs taking 100 images in batches
-# total improvement is printed out after each epoch
+# RUNNING THE MODEL FOR A 1000 EPOCHS TAKING 100 IMAGES IN BATCHES
+# TOTAL IMPROVEMENT IS PRINTED OUT AFTER EACH EPOCH
 
 for epoch in range(hm_epochs):
     epoch_loss = 0    # initializing error as 0
@@ -91,22 +88,19 @@ print('Epoch', epoch, '/', hm_epochs, 'loss:',epoch_loss)
 
 
 
+# PICK ANY IMAGE
+any_image = all_images[900]
 
-
-
-
-
-# pick any image
-any_image = all_images[999]
-# run it though the autoencoder
+# RUN IT THROUGH THE AUTOENCODER
 output_any_image = sess.run(output_layer,feed_dict={input_layer:[any_image]})
 
-# run it though just the encoder
+# RUN IT THROUGH JUST THE ENCODER
 encoded_any_image = sess.run(layer_1,feed_dict={input_layer:[any_image]})
 
-# print the original image
+# PRINT THE ORIGINAL IMAGE
 
 plt.imshow(any_image(28,28),  cmap='Greys')
 plt.show()
-# print the encoding
+
+# PRINT THE ENCODING
 print(encoded_any_image)
